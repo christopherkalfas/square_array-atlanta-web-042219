2f9a61a58c9f110e792af457834169cbe6030ce1
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 19:59:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,15 @@
-def square_array(array)
-  squared_array = []
-  array.each do |number|
-    squared_number = number * number
-    squared_array << squared_number
-  end 
-  return squared_array
-end
+# def square_array(array)
+#   squared_array = []
+#   array.each do |number|
+#     squared_number = number * number
+#     squared_array << squared_number
+#   end 
+#   return squared_array
+# end
 
 def squared_array(array)
   array.collect do |num|
-    
+    num * num
+  end 
+end 
+  
