fea7da85f6f6695e352b1369103d1992f76949bf
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 20:05:43 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,11 +1,7 @@
 describe '#square_array' do
 
  
-  it 'calls on each' do
-    numbers = [1,2,3]
-    expect(numbers).to receive(:each)
-    square_array(numbers)
-  end
+  
 
   it 'should square the elements in an array' do
     expect(square_array([1,2,3])).to eq([1,4,9])
